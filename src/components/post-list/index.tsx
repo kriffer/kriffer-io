6c586b13daba00e633277232d6260c5acd8d5a58
kriffer <antonio.kravets@gmail.com>
2022-01-26 14:43:36 +0200

    sort posts by date

--- src/components/post-list/index.tsx
+++ src/components/post-list/index.tsx
@@ -20,7 +20,7 @@ const PostList: React.FC<PostListProps> = ({posts}) => {
     return (
         <div>
             <Menu secondary vertical fluid>
-                {posts.map(post => (
+                {posts.sort((a, b) => {return  new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()}).map(post => (
                     <Menu.Item key={post.postId} onClick={() => handleClick(post.slug)}>
                         <Header size={'large'}>{post.summary} </Header>
                         <p>{new Date(post.createdAt).toLocaleDateString()}</p>
